01dcec3c2814aa9154602a3f894213da1c7f3386
unknown <anigandini@gmail.com>
2026-05-14 08:14:16 -0300

    feat: contact section desktop

--- app/components/sections/ContactSection.vue
+++ app/components/sections/ContactSection.vue
@@ -1,1 +1,82 @@
-<template></template>
+<template>
+    <section class="services-section flex flex-col mt-30 p-25 pb-10">
+        <h3 class="title text-5xl">HAVE A PROJECT IN MIND?<br>LET’S TALK</h3>
+        <main>
+            <ul class="flex flex-col gap-10">
+                <li class="flex items-center gap-5" v-for="link in links" :key="link.name">
+                    <button
+                        v-if="link.copy"
+                        @click="copyEmail(link.value)"
+                        class="text-xl cursor-pointer"
+                    >
+                        {{ copied ? 'Email copied!' : link.name }}
+                    </button>
+
+                    <a
+                        v-else
+                        :href="link.href"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        class="text-xl"
+                    >
+                        {{ link.name }}
+                    </a>  
+                </li>
+                
+            </ul>
+        </main>
+        <footer>@2026 Anahí</footer>
+    </section>
+</template>
+
+<style scoped>
+.services-section {
+    background-color: var(--color-black);
+    color: var(--color-white);
+}
+
+ul {
+    align-items: flex-end;
+}
+
+button:hover {
+    color: var(--color-primary);
+}
+
+</style>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const copied = ref(false)
+
+const copyEmail = async (email: string) => {
+    try {
+        await navigator.clipboard.writeText(email)
+
+        copied.value = true
+
+        setTimeout(() => {
+            copied.value = false
+        }, 2000)
+    } catch (error) {
+        console.error('Failed to copy email:', error)
+    }
+}
+
+const links = [
+    {
+        name: 'Email',
+        value: 'anigandini@gmail.com',
+        copy: true
+    },
+    {
+        name: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/anigandini'
+    },
+    {
+        name: 'GitHub',
+        href: 'https://github.com/anigandini'
+    }
+]
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,12 +2,13 @@
   <HeroSection />
   <AboutSection />
   <ServicesSection />
-   <!--<BlogSection />
-  <ContactSection />-->
+   <!--<BlogSection />-->
+  <ContactSection />
 </template>
 
 <script setup>
 import HeroSection from '@/components/sections/HeroSection.vue';
 import AboutSection from '@/components/sections/AboutSection.vue';
 import ServicesSection from '@/components/sections/ServicesSection.vue';
+import ContactSection from '@/components/sections/ContactSection.vue';
 </script>
